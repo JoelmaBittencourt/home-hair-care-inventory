93f2b3de12b2eba2839b510bf5d10fa32de3c119
joelma <joelmadev@gmail.com>
2023-06-21 17:19:16 -0300

    github actions

---
+++ src/main/java/com/homeHairCareInventory/HomeHairCareInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.homeHairCareInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHairCareInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeHairCareInventoryApplication.class, args);
+	}
+
+}
